5f0f28b1202afdddcb3eabcf57335a01972012ae
Charles de MAGNEVAL <magneval@gmail.com>
2013-04-20 21:13:26 +0200

    Mise a jour du jeu avant la synchro avec Github

---
+++ src/main/webapp/byCriteria.js
@@ -0,0 +1,7 @@
+{
+  identifier: 'idproduct',
+  items:[
+  {"price":"1","idproduct":"216","reference":"Test","libelle":"Essai","dprice":"1"},
+  {"price":"100","idproduct":"217","reference":"test2","libelle":"ça fonctionne","dprice":"1000"}]
+}
+
